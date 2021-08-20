47a40ff1ee6ee2ec2ad97b9f1c4d8ca3b084efd1
IndraP24 <indrashis985@gmail.com>
2021-08-20 09:40:22 +0530

    Update script.js for multiple files

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 function performOCR() {
-  var files = document.getElementById("image_file").files;
+  var files = document.getElementById("image_file").files
   var formData = new FormData();
   var endpoint = "/extract_text";
   if (files.length == 1) {
     formData.append("image", files[0])
   }
   else {
-    for (var i = 1; i < files.length; i++) {
+    for (var i = 0; i < files.length; i++) {
       formData.append("image" + i.toString(), files[i]);
     }
     endpoint = "/bulk_extract_text";
@@ -25,16 +25,45 @@ function performOCR() {
         swal("Converted Text", data.text);
       }
       else {
-        swal("Request Received", "Converted files will start showing up at the bottom soon!")
+        swal("Request Received", "Converted files will start showing up at the bottom soon!");
+        getConvertedFiles(data.task_id, data.num_files);
       }
-
     }
   });
 }
 
 
-function getConvertedFiles(task_id, numFiles) {
-  var checker = setInterval(function()) {
+function getConvertedFiles(taskID, numFiles) {
+  var checker = setInterval(function() {
+    $.ajax({
+        type: 'GET',
+        url: '/bulk_output/' + taskID,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function(data) {
+          wrapper = document.getElementById("bulk_result")
+          for (var key in data.output) {
+            var element = document.createElement("button");
+            element.setAttribute("class", "btn btn-primary")
+            element.setAttribute("info", data.output[key])
+            element.setAttribute("id", key)
+            element.setAttribute("onclick", "displayText(this.id)")
+            element.innerHTML = key
+            wrapper.appendChild(element)
+          }
+          if (Object.keys(data.output).length == numFiles) {
+            stopChecker()
+          }
+        }
+    });
+  }, 3000);
 
+  function stopChecker() {
+    clearInterval(checker)
   }
+}
+
+function displayText(id) {
+  swal("Converted Text", document.getElementById(id).getAttribute("info"))
 }
